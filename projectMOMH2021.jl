cb2662ad5950f14af46bfe3fb1a11e0675fe0aa8
gandibleux-x <xavier.gandibleux@univ-nantes.fr>
2021-10-31 20:37:12 +0100

    vOptMomkp

--- projectMOMH2021.jl
+++ projectMOMH2021.jl
@@ -5,9 +5,15 @@
 # ==============================================================================
 include("parserMomkpZL.jl")
 include("parserMomkpPG.jl")
+include("vOptMomkp.jl")
 
 # ==============================================================================
 # Datastructure of a generic bi-objective 0/1 IP where all coefficients are integer
+#
+#   Max  sum{j=1,...,n} C(k,j) x(j)                k=1,...,p
+#   st   sum{j=1,...,n} A(i,j) x(j) <= b_{i}       i=1,...,m
+#                       x(j) = 0 or 1
+
 struct _bi01IP
     C  :: Matrix{Int} # objective functions, k=1..2, j=1..n
     A  :: Matrix{Int} # matrix of constraints, i=1..m, j=1..n
@@ -16,6 +22,7 @@ end
 
 # ==============================================================================
 
+# Example ----------------------------------------------------------------------
 fname = "instancesZL/knapsack.100.3"
 verbose = true
 
@@ -25,7 +32,7 @@ momkpZL = readInstanceMOMKPformatZL(verbose,fname)
 # Reduce the MO-MKP instance to the two first objectives and store it as a generic Bi-01IP
 dat1 = _bi01IP(momkpZL.P[1:2,:], momkpZL.W, momkpZL.ω)
 
-
+# Example ----------------------------------------------------------------------
 fname = "instancesPG/set1/ZL28.DAT"
 #fname = "instancesPG/set2/kp28W-Perm.DAT"
 #fname = "instancesPG/set3/W7BI-tube1-1800.DAT"
@@ -36,3 +43,18 @@ momkpPG = readInstanceMOMKPformatPG(verbose,fname)
 
 # Store it as a generic bi-01IP
 dat2 = _bi01IP(momkpPG.P, momkpPG.W, momkpPG.ω)
+
+# Example ----------------------------------------------------------------------
+solverSelected = GLPK.Optimizer
+Y_N, X_E = vSolveBi01IP(solverSelected, dat2.C, dat2.A, dat2.b)
+
+# ---- Displaying the results (X_E and Y_N)
+for n = 1:length(Y_N)
+    X = value.(X_E, n)
+    print(findall(elt -> elt ≈ 1, X))
+    println("| z = ",Y_N[n])
+end
+
+for n = 1:length(Y_N)
+    println(Y_N[n])
+end
---
+++ vOptMomkp.jl
@@ -0,0 +1,43 @@
+using vOptGeneric, JuMP, GLPK
+
+# Setting a Bi01IP instance and solving with vOptGeneric
+function vSolveBi01IP(solverSelected, C, A, B)
+
+  m, n = size(A)
+
+  # ---- setting the model
+  println("Building...")
+  Bi01IP = vModel( solverSelected )
+  @variable( Bi01IP, x[1:n], Bin )
+  @addobjective( Bi01IP, Max, sum(C[1,j] * x[j] for j=1:n) )
+  @addobjective( Bi01IP, Max, sum(C[2,j] * x[j] for j=1:n) )
+  @constraint( Bi01IP, cte[i=1:m], sum(A[i,j] * x[j] for j=1:n) <= B[i])
+
+  # ---- Invoking the solver (epsilon constraint method)
+  println("Solving...")
+  vSolve( Bi01IP, method=:epsilon, step=0.5, verbose=true )
+
+  # ---- Querying the results
+  println("Querying...")
+  Y_N = getY_N( Bi01IP )
+
+  return Y_N, x
+end
+
+# ==============================================================================
+
+# Example on how to use it :
+
+#solverSelected = GLPK.Optimizer
+#Y_N, X_E = setBi01IP(solverSelected, C, A, B)
+
+# ---- Displaying the results (X_E and Y_N)
+#for n = 1:length(Y_N)
+#    X = value.(X_E, n)
+#    print(findall(elt -> elt ≈ 1, X))
+#    println("| z = ",Y_N[n])
+#end
+
+#for n = 1:length(Y_N)
+#    println(Y_N[n])
+#end
